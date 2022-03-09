e2b290b370ea814cd695e32e95d0ea35921d3d50
GuilhermeRS92 <guilhermeribeirosouza92@gmail.com>
2022-03-08 23:33:20 -0300

    main

---
+++ gestaoColaboradores/gestaoColaboradores/src/app/feature-colaboradores/feature-colaboradores.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListaColaboradoresComponent } from './lista-colaboradores/lista-colaboradores.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListaColaboradoresComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeatureColaboradoresModule { }
